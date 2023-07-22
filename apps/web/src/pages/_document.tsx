1151697f3442c38e8ac1738600d869d482154c12
William Villeneuve <ciiqr@users.noreply.github.com>
2023-07-22 01:29:03 -0400

    setup basic ui

--- apps/web/src/pages/_document.tsx
+++ apps/web/src/pages/_document.tsx
@@ -6,40 +6,10 @@ export default function Document() {
             <Head>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <body className="text-gray-200">
-                <Background />
+            <body className="bg-slate-200 text-slate-950">
                 <Main />
                 <NextScript />
             </body>
         </Html>
-    );
-}
-
-function Background() {
-    return (
-        <div className="fixed -z-10 h-full w-full bg-gradient-to-t from-slate-800 to-slate-900">
-            <div
-                className="h-full w-full [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]"
-                style={{
-                    backgroundSize: "40px 40px",
-                    backgroundImage: `
-                        linear-gradient(to right, rgba(255,255,255,.02) 1px, transparent 1px),
-                        linear-gradient(to bottom, rgba(255,255,255,.02) 1px, transparent 1px)
-                    `,
-                }}
-            />
-            {/* <div
-                className="fixed -left-1/2 -top-1/2 -rotate-12"
-                style={{
-                    width: "200%",
-                    height: "200%",
-                    backgroundSize: "314px 314px",
-                    backgroundImage: `
-                        linear-gradient(to right, rgba(255,255,255,.02) 1px, transparent 1px),
-                        linear-gradient(to bottom, rgba(255,255,255,.02) 1px, transparent 1px)
-                    `,
-                }}
-            /> */}
-        </div>
     );
 }
--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,7 +1,23 @@
+import { TrashIcon } from "@radix-ui/react-icons";
 import Head from "next/head";
+import { useState } from "react";
 
 export default function Home() {
-    const fonts = [{ id: "nope" }];
+    const [text, setText] = useState("Aa");
+    const fonts = [
+        { id: "nope" },
+        { id: "yep" },
+        { id: "yep-1" },
+        { id: "yep-2" },
+        { id: "yep-3" },
+        { id: "yep-4" },
+        { id: "yep-5" },
+        { id: "yep-6" },
+        { id: "yep-7" },
+        { id: "yep-8" },
+        { id: "yep-9" },
+        { id: "yep-10" },
+    ];
 
     return (
         <>
@@ -9,16 +25,41 @@ export default function Home() {
                 <title>font-finder</title>
                 <link rel="icon" href="/favicon.png" />
             </Head>
-            <main className="container mx-auto flex flex-col items-center p-4">
-                {fonts.length > 0 ? (
-                    fonts.map((font) => (
-                        <p key={font.id} className="text-2xl">
-                            {font.id}
-                        </p>
-                    ))
-                ) : (
-                    <p className="text-2xl">no guys?</p>
-                )}
+            <main className="flex flex-col p-1">
+                <input
+                    className="border-2 border-slate-500 bg-slate-50 p-2"
+                    value={text}
+                    onChange={(e) => setText(e.target.value)}
+                />
+                <div className="flex flex-row flex-wrap justify-center gap-1 py-1">
+                    {fonts.length > 0 ? (
+                        fonts.map((font) => (
+                            <div
+                                key={font.id}
+                                className="relative flex aspect-square w-32 items-center justify-center overflow-hidden border-2 border-slate-500"
+                                title={font.id}
+                            >
+                                <span
+                                    key={font.id}
+                                    className=" text-center text-2xl"
+                                >
+                                    {text}
+                                </span>
+                                <button
+                                    className="absolute bottom-1 right-1 rounded-sm bg-red-500 p-1 text-white"
+                                    type="button"
+                                    onClick={() => {
+                                        console.log("delete", font.id);
+                                    }}
+                                >
+                                    <TrashIcon />
+                                </button>
+                            </div>
+                        ))
+                    ) : (
+                        <p className="text-2xl">no guys?</p>
+                    )}
+                </div>
             </main>
         </>
     );
